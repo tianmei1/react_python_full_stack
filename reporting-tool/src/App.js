220b0938074778edd22ba0b9593f458340fcffde
tianmei1 <tmei1@hawk.iit.edu>
2024-10-12 13:45:26 -0500

    remove unused

--- reporting-tool/src/App.js
+++ reporting-tool/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
 import ReportBuilder from './ReportBuilder';
-import RandomDataButton from './RandomDataButton';
 
 function App() {
   const [layout, setLayout] = useState({
@@ -16,7 +15,6 @@ function App() {
     <div>
       <h1>Report Layout Builder Tool</h1>
       <ReportBuilder layout={layout} setLayout={setLayout} />
-      <RandomDataButton setLayout={setLayout} />
     </div>
   );
 }
